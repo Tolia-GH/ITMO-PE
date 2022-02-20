96c49346c4e369b887e9a6692d9f12f8c359cca9
Tolia <45510494+Tolia-GH@users.noreply.github.com>
2022-02-20 21:52:38 +0300

    update

--- Program/Prog_Lab_5/src/Collection/Organization.java
+++ Program/Prog_Lab_5/src/Collection/Organization.java
@@ -1,6 +1,11 @@
 package Collection;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Organization {
+    public static int idCode = 0;
+
     private long id; //Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
     private String name; //Поле не может быть null, Строка не может быть пустой
     private Coordinates coordinates; //Поле не может быть null
@@ -10,4 +15,64 @@ public class Organization {
     private Long employeesCount; //Поле может быть null, Значение поля должно быть больше 0
     private OrganizationType type; //Поле может быть null
     private Address postalAddress; //Поле не может быть null
+
+    public int getIdCode() {
+        return idCode;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public Long getAnnualTurnover() {
+        return annualTurnover;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public Long getEmployeesCount() {
+        return employeesCount;
+    }
+
+    public OrganizationType getType() {
+        return type;
+    }
+
+    public Address getPostalAddress() {
+        return postalAddress;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Organization that = (Organization) o;
+        return id == that.id &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(coordinates, that.coordinates) &&
+                Objects.equals(creationDate, that.creationDate) &&
+                Objects.equals(annualTurnover, that.annualTurnover) &&
+                Objects.equals(fullName, that.fullName) &&
+                Objects.equals(employeesCount, that.employeesCount) &&
+                type == that.type && Objects.equals(postalAddress, that.postalAddress);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, coordinates, creationDate, annualTurnover, fullName, employeesCount, type, postalAddress);
+    }
 }
--- Program/Prog_Lab_5/src/Main.java
+++ Program/Prog_Lab_5/src/Main.java
@@ -1,3 +1,8 @@
+/**
+ * @author P3113 Чжоу Хунсян
+ * Variant 951316
+ */
+
 public class Main {
     public static void main(String[] args) {
 
