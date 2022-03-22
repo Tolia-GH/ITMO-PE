bf3625c8ca3bf0b1a12590f46f8ab8f22430ab7f
Tolia <45510494+Tolia-GH@users.noreply.github.com>
2022-03-22 15:28:06 +0300

    update

--- Program/Prog_Lab_5/src/Collection/Coordinates.java
+++ Program/Prog_Lab_5/src/Collection/Coordinates.java
@@ -3,4 +3,8 @@ package Collection;
 public class Coordinates {
     private Float x; //Значение поля должно быть больше -295, Поле не может быть null
     private double y; //Максимальное значение поля: 500
+
+    public Coordinates(Integer x,Integer y) {
+        
+    }
 }
