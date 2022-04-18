8de52ff0809b060ed62b651620dfd7ac11866a7b
Tolia <45510494+Tolia-GH@users.noreply.github.com>
2022-04-18 22:05:45 +0300

    Update Main.java

--- Program/Prog_Lab_5/src/Main.java
+++ Program/Prog_Lab_5/src/Main.java
@@ -17,7 +17,6 @@ public class Main {
      */
     public static void main(String[] args) {
 
-
         CommandManager commandManager = new CommandManager();
 
         Iterator<AbstractCommand> iterator;
