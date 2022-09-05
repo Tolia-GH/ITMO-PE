b3650d8b7f17ed337a97c384d59fc1394ba222cd
Tolia <45510494+Tolia-GH@users.noreply.github.com>
2022-09-05 16:16:12 +0300

    update

--- Program/Prog_Lab_7/src/Tools/SQL.java
+++ Program/Prog_Lab_7/src/Tools/SQL.java
@@ -1,15 +1,11 @@
 package Tools;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.Files;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SQL {
-    public static void main(String args[]) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
         try(Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5433/postgres","postgres","123456")){
             System.out.print(connection);
