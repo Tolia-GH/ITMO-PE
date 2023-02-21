4c694e848ab06c7f54b62ac17980951cc84ccbdb
Tolia <2398768715@qq.com>
2023-02-21 15:20:20 +0300

    Update module-info.java

--- Mathematic/GraphicWork1/GraphicWork-1/src/main/java/module-info.java
+++ Mathematic/GraphicWork1/GraphicWork-1/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.graphicwork {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.graphicwork to javafx.fxml;
     exports com.example.graphicwork;
 }
