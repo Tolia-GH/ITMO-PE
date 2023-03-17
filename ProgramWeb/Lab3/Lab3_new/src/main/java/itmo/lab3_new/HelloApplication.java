50ef8794da9b6440254d0d7434e4b9804d4689f2
Tolia <2398768715@qq.com>
2023-03-17 20:21:42 +0300

    update

--- ProgramWeb/Lab3/Lab3_new/src/main/java/itmo/lab3_new/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package itmo.lab3_new;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
