aa96f27b1031c605a77652cbd26b97b4414af639
Tolia <2398768715@qq.com>
2023-03-22 15:23:06 +0300

    update

--- ProgramWeb/Lab3/src/main/java/DataBase/DataBaseStorage.java
+++ ProgramWeb/Lab3/src/main/java/DataBase/DataBaseStorage.java
@@ -41,9 +41,9 @@ public class DataBaseStorage implements Serializable {
     public void addNewDot(DotsTable dot) throws Exception{
         String url = "jdbc:postgresql://localhost:5432/postgres";
         Class.forName("org.postgresql.Driver");
-        try(Connection connection = DriverManager.getConnection(url)){
+        try(Connection connection = DriverManager.getConnection(url,"postgres","123456")){
             try(PreparedStatement ps= connection.prepareStatement(
-                    "INSERT INTO dotstable(hit,r,time,x,y) values (?,?,?,?,?)"
+                    "INSERT INTO web_lab3.dotstable(hit,r,time,x,y) values (?,?,?,?,?)"
             )){
                 ps.setBoolean(1,dot.isHit());
                 ps.setDouble(2,dot.getR());
@@ -52,11 +52,11 @@ public class DataBaseStorage implements Serializable {
                 ps.setDouble(5,dot.getY());
                 ps.executeUpdate();
             }catch (SQLException e){
-                //e.printStackTrace();
+                e.printStackTrace();
             }
         }catch (SQLException e){
-            //e.printStackTrace();
-        };
+            e.printStackTrace();
+        }
     }
     public void deleteDot(DotsTable dot){
         Session session = getSessionFactory().openSession();
