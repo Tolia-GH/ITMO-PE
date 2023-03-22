dcc02e618b61e6f93ffba22253779da12443594c
Tolia <2398768715@qq.com>
2023-03-22 16:15:12 +0300

    create lab4 spring

---
+++ ProgramWeb/Lab4/lab4_spring/src/main/java/com/itmo/lab4_spring/Lab4SpringApplication.java
@@ -0,0 +1,13 @@
+package com.itmo.lab4_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4SpringApplication.class, args);
+    }
+
+}
---
+++ ProgramWeb/Lab4/lab4_spring/src/test/java/com/itmo/lab4_spring/Lab4SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmo.lab4_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
