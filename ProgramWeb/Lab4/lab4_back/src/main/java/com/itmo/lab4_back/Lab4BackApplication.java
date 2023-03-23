856e939d43e1dffe70cb6b07e0083fbb5bfcbcdd
Tolia <2398768715@qq.com>
2023-03-23 13:35:31 +0300

    recreate Lab4

---
+++ ProgramWeb/Lab4/lab4_back/src/main/java/com/itmo/lab4_back/Lab4BackApplication.java
@@ -0,0 +1,13 @@
+package com.itmo.lab4_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4BackApplication.class, args);
+    }
+
+}
---
+++ ProgramWeb/Lab4/lab4_back/src/test/java/com/itmo/lab4_back/Lab4BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmo.lab4_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
