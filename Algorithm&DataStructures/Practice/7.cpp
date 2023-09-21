a170bb984c346a0486f7952b1af698979e919612
托利亚 <2398768715@qq.com>
2023-09-21 21:52:51 +0300

    update

---
+++ Algorithm&DataStructures/Practice/7.cpp
@@ -0,0 +1,111 @@
+#include "string"
+#include "iostream"
+#include "cstring"
+
+using namespace std;
+
+int weight[26];
+char stringS[100001];
+char resultS[100001];
+char topLetters[100001];
+int resultIdx = 0;
+int topIndex = 0;
+
+int compare(char a, char b) {//if weight of b > a then res > 0
+    if(weight[a - 'a']!=weight[b - 'a']){
+        return weight[b - 'a'] - weight[a - 'a'];
+    }
+    return b - a;
+}
+
+void bubbleSort(char arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (compare(arr[j], arr[j + 1]) > 0) {
+                char temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+void quickSort(char arr[], int l, int r) {
+    if (l < r) {
+        int i = l, j = r, c = arr[l];
+        while (i < j) {
+            while (i < j && compare(arr[j], c) >= 0) {
+                j--;
+            }
+            if (i < j) {
+                arr[i++] = arr[j];
+            }
+            while (i < j && compare(arr[i], c) < 0) {
+                i++;
+            }
+            if (i < j) {
+                arr[j--] = arr[i];
+            }
+        }
+        arr[i] = c;
+        quickSort(arr, l, i - 1);
+        quickSort(arr, i + 1, r);
+    }
+}
+
+void printRes() {
+    for (int i = 0; i < topIndex; i++) {
+        cout << topLetters[i];
+    }
+
+    for (int i = 0; i < resultIdx; i++) {
+        cout << resultS[i];
+    }
+
+    for (int i = topIndex - 1; i >= 0; i--) {
+        cout << topLetters[i];
+    }
+}
+
+int main() {
+    cin >> stringS;
+
+    int lengthS = strlen(stringS);
+
+    for (int & i : weight) {
+        cin >> i;
+    }
+
+    //bubbleSort(stringS, lengthS);
+    quickSort(stringS, 0, lengthS);
+
+//    for (int i = 0; i < lengthS; ++i) {
+//        cout << stringS[i];
+//
+//    }
+//    cout << endl;
+
+    for(int i = 0; i< lengthS - 1; i++) {
+        if (stringS[i + 1] == stringS[i]) {
+            if (topIndex == 0 || topLetters[topIndex - 1] != stringS[i]) {
+                topLetters[topIndex] = stringS[i];
+                topIndex++;
+                ++i;
+            } else {
+                resultS[resultIdx] = stringS[i];
+                resultIdx++;
+            }
+        } else {
+            resultS[resultIdx] = stringS[i];
+            resultIdx++;
+        }
+    }
+
+    if (lengthS != resultIdx + 2 * topIndex) {
+        resultS[resultIdx++] = stringS[lengthS - 1];
+    }
+
+    printRes();
+
+    return 0;
+}
--- Algorithm&DataStructures/Test/main.cpp
+++ Algorithm&DataStructures/Test/main.cpp
@@ -1,47 +1,80 @@
-#include <string>
-#include "iostream"
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
 
 using namespace std;
 
-
-int compare(const string &a, const string &b) {
-    string temp1 = a + b;
-    string temp2 = b + a;
-
-    return temp1.compare(temp2);
+int cmp(const void* a, const void* b) {
+    return *(int*)b - *(int*)a;
 }
 
-void bubbleSort(string arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (compare(arr[j], arr[j + 1]) < 0) {
-                swap(arr[j], arr[j + 1]);
+inline void read(int &num) {
+    num = 0;
+    short flag = 1;
+    char ch = getchar();
+    while(ch < '0' || ch > '9') {
+
+        if(ch == '-')flag = -1;
+        ch = getchar();
+    }
+    while(ch >= '0' && ch <= '9') {
+        num = (num << 3) + (num << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    num *= flag;
+}
+
+inline void write(int num) {
+    if (num == 0) return;
+    write(num / 10);
+    putchar(num % 10 + '0');
+}
+
+void quickSort(int arr[], int l, int r) {  //from big to small
+    if (l < r) {
+        int i = l, j = r, c = arr[l];
+        while (i < j) {
+            while (i < j && c >= arr[j]) {
+                j--;
+            }
+            if (i < j) {
+                arr[i++] = arr[j];
+            }
+            while (i < j && c < arr[i]) {
+                i++;
+            }
+            if (i < j) {
+                arr[j--] = arr[i];
             }
         }
+        arr[i] = c;
+        quickSort(arr, l, i - 1);
+        quickSort(arr, i + 1, r);
     }
 }
 
 int main() {
-    string nums[100] = {};
+    int n;  // number of things
+    int k;  // every k-th thing free
+    int prices[100001];  //price of all things
+    int sum = 0;
 
-    int count = 0;
-    while(true) {
-        cin >> nums[count];
+    read(n);
+    read(k);
 
-        if (nums[count].empty()) {
-            break;
+    for (int i = 0; i < n; i++) {
+        read(prices[i]);
+    }
+
+    qsort(prices, n, sizeof(int), cmp);
+
+    for (int i = 0; i < n; i++) {
+        if((i + 1) % k != 0) {
+            sum += prices[i];
         }
-
-        count ++;
     }
 
-    bubbleSort(nums, count);
-
-    //qsort(nums, 3, sizeof(char *), compare);
-
-    for (int i = 0; i < count; i++) {
-        cout << nums[i];
-    }
+    printf("%d", sum);
 
     return 0;
 }
