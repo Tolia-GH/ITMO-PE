9c57980623682a0d03a1d71a50a4a967c5162241
Tolia <2398768715@qq.com>
2023-10-10 22:18:56 +0300

    update

--- AI/Labs/Lab1.pl
+++ AI/Labs/Lab1.pl
@@ -1,4 +1,4 @@
-% define characters `Character(name).`
+% define characters `character(name).`
 character(noelle).
 character(klee).
 character(ganyu).
@@ -8,70 +8,131 @@ character(kokomi).
 character(nahida).
 character(nilou).
 character(lynette).
-character(furina).
+character(neuvilette).
 
-% define the nation of characters `Nation(characterName, nation).`
-nation(noelle, Moundstalt).
-nation(klee, Moundstalt).
-nation(Ganyu, Liyue).
-nation(Keqing, Liyue).
-nation(Ayaka, Inazuma).
-nation(Kokomi, Inazuma).
-nation(Nahada, Sumeru).
-nation(Nilou, Sumeru).
-nation(Lynette, Fontaine).
-nation(Furina, Fontaine).
+% define the nation of characters `nation(characterName, nation).`
+nation(noelle, moundstalt).
+nation(klee, moundstalt).
+nation(ganyu, liyue).
+nation(keqing, liyue).
+nation(ayaka, inazuma).
+nation(kokomi, inazuma).
+nation(nahida, sumeru).
+nation(nilou, sumeru).
+nation(lynette, fontaine).
+nation(neuvilette, fontaine).
 
-% define HP of character `HP(characterName, hitpoints).`
-HP(Noelle, ).
-HP(Klee, ).
-HP(Ganyu, ).
-HP(Keqing, ).
-HP(Ayaka, ).
-HP(Kokomi, ).
-HP(Nahada, ).
-HP(Nilou, ).
-HP(Lynette, ).
-HP(Furina, ).
+% define HP of character `hp(characterName, hitpoints).`
+hp(noelle, 12071).
+hp(klee, 10287).
+hp(ganyu, 9797).
+hp(keqing, 13103).
+hp(ayaka, 12858).
+hp(komiomi, 13471).
+hp(nahida, 10360).
+hp(nilou, 15185).
+hp(lynette, 12937).
+hp(neuvilette, 14695).
 
-% define ATK of character `ATK(characterName, ATK).`
-ATK(Noelle, ).
-ATK(Klee, ).
-ATK(Ganyu, ).
-ATK(Keqing, ).
-ATK(Ayaka, ).
-ATK(Kokomi, ).
-ATK(Nahada, ).
-ATK(Nilou, ).
-ATK(Lynette, ).
-ATK(Furina, ).
+% define ATK of character `atk(characterName, atk).`
+atk(noelle, 701).
+atk(klee, 352).
+atk(ganyu, 943).
+atk(keqing, 323).
+atk(ayaka, 796).
+atk(komiomi, 799).
+atk(nahida, 626).
+atk(nilou, 348).
+atk(lynette, 232).
+atk(neuvilette, 208).
 
-% define elementType of character `ElementType(characterName, element).`
-elementType(Noelle, Geo).
-elementType(Klee, Pyro).
-elementType(Ganyu, Cryo).
-elementType(Keqing, Electro).
-elementType(Ayaka, Cryo).
-elementType(Kokomi, Hydro).
-elementType(Nahida, Dendro).
-elementType(Nilou, Hydro).
-elementType(Lynette, Anemo).
-elementType(Furina, Hydro).
+% define elementType of character `elementType(characterName, element).`
+elementType(noelle, geo).
+elementType(klee, pyro).
+elementType(ganyu, cryo).
+elementType(keqing, electro).
+elementType(ayaka, cryo).
+elementType(kokomi, hydro).
+elementType(nahida, dendro).
+elementType(nilou, hydro).
+elementType(lynette, anemo).
+elementType(neuvilette, hydro).
 
-% define weaponType of character `WeaponType(characterName, weaponType).`
-useWeaponType(Noelle, ).
-useWeaponType(Klee, ).
-useWeaponType(Ganyu, ).
-useWeaponType(, ).
-useWeaponType(, ).
-useWeaponType(, ).
-useWeaponType(, ).
-useWeaponType(, ).
-useWeaponType(, ).
-useWeaponType(, ).
-useWeaponType(, ).
+% define weaponType of character `useWeaponType(characterName, weaponType).`
+useWeaponType(noelle, claymore).
+useWeaponType(klee, catalyst).
+useWeaponType(ganyu, bow).
+useWeaponType(keqing, sword).
+useWeaponType(ayaka, sword).
+useWeaponType(kokomi, catalyst).
+useWeaponType(nahida, catalyst).
+useWeaponType(nilou, sword).
+useWeaponType(lynette, sword).
+useWeaponType(neuvilette, catalyst).
 
-% define `Weapon(name, weaponType).`
-weapon(, ).
+% define weapons `weapon(name).`
+weapon(bloodtaintedGreatsword). % 沐浴龙血的剑
+weapon(rainslasher). % 雨裁
+weapon(magicGuide). % 魔法导论
+weapon(skywardAtlas). % 天空之卷
+weapon(oathswornEye). % 证誓之明瞳
+weapon(solarPearl). % 匣里日月
+weapon(tomeOfTheEternalFlow). % 万世流涌大典
+weapon(amosbow). % 阿莫斯之弓
+weapon(fadingTwilight). % 落霞
+weapon(silverSword). % 银剑
+weapon(filletBlade). % 吃虎鱼刀
+weapon(amenomaKageuchi). % 天目影打刀
 
-% define equipment of characters `Equipment(characterName, weaponName).`
+% define weaponType `weaponType(weaponName, weaponType).`
+weaponType(bloodtaintedGreatsword, claymore).
+weaponType(rainslasher, claymore).
+weaponType(magicGuide, catalyst).
+weaponType(skywardAtlas, catalyst).
+weaponType(oathswornEye, catalyst).
+weaponType(solarPearl, catalyst).
+weaponType(tomeOfTheEternalFlow, catalyst).
+weaponType(amosbow, bow).
+weaponType(fadingTwilight, bow).
+weaponType(silverSword, sword).
+weaponType(filletBlade, sword).
+weaponType(amenomaKageuchi, sword).
+
+% define enemies `enemy(enemyName).`
+enemy(slime).
+enemy(hilichurl).
+enemy(ruinGuard).
+enemy(treasureHoarders).
+enemy(nobushi).
+enemy(eremite).
+
+% define Boss `Boss(BossName).`
+boss(stormterror).
+boss(childe).
+boss(azhdaha).
+
+% define HP of enemies `hp(enemyName, hp).`
+hp(slime, hp).
+hp(hilichurl, hp).
+hp(ruinGuard, hp).
+hp(treasureHoarders, hp).
+hp(nobushi, hp).
+hp(eremite, hp).
+
+% define HP of boss `hp(bossName, hp).`
+hp(stormterror, hp).
+hp(childe, hp).
+hp(azhdaha, hp).
+
+% define atk of enemies `atk(enemyName, atk).`
+atk(slime, atk).
+atk(hilichurl, atk).
+atk(ruinGuard, atk).
+atk(treasureHoarders, atk).
+atk(nobushi, atk).
+atk(eremite, atk).
+
+% define atk of boss `atk(bossName, atk).`
+atk(stormterror, atk).
+atk(childe, atk).
+atk(azhdaha, atk).
--- temp.md
+++ temp.md
@@ -1,32 +1,12 @@
-```prolog
-% define characters %
-character(name).
+% define atk of enemies `atk(enemyName, atk).`
+atk(enemyName, atk).
+atk(enemyName, atk).
+atk(enemyName, atk).
+atk(enemyName, atk).
+atk(enemyName, atk).
+atk(enemyName, atk).
 
-% define the nation of characters 
-nation(characterName, nation).
-
-% define HP of character 
-hp(characterName, Hitpoints).
-
-% define ATK of character 
-atk(characterName, ATK).
-
-% define elementType of character 
-elementType(characterName, element).
-
-% define weaponType of character 
-weaponType(characterName, weaponType).
-
-% define 
-weapon(weponName, weaponType).
-
-% define equipment of characters 
-equipment(characterName, weaponName).
-```
-
-define rules
-```prolog
-canEquip(CharacterName, WeaponName) :-
-    weaponType(CharacterName, weaponType)
-    weapon(WeponName, weaponType)
-```
+% define atk of boss `atk(bossName, atk).`
+atk(enemyName, atk).
+atk(enemyName, atk).
+atk(enemyName, atk).
