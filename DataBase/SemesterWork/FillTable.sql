62263bb422f117face9c68171c233497cd7fba79
Tolia <2398768715@qq.com>
2024-03-06 03:40:57 +0300

    update

--- DataBase/SemesterWork/FillTable.sql
+++ DataBase/SemesterWork/FillTable.sql
@@ -3,7 +3,7 @@ DECLARE
 i int = 0;
 names varchar(64)[] = array ['Peter','Bob','John','Tomas','Alex','Anna'];
 BEGIN
-    while i < 100000 loop
+    while i < 1000000 loop
             insert into "user"(username,gender,password,age) values (names[i%6+1],'MALE',random()*1000,28);
             i = i + 1;
 end loop;
@@ -62,7 +62,7 @@ DECLARE
     i int = 0;
     houses house_type[] = array ['APARTMENTS', 'VILLAS', 'HIGH-END','ORDINARY'];
 BEGIN
-    while i < 100000 loop
+    while i < 1000000 loop
             insert into house(address_id, house_type) VALUES (i+1,houses[i%4+1]);
             i = i + 1;
         end loop;
@@ -76,7 +76,7 @@ DECLARE
     i int = 0;
     rooms room_type[] = array ['KITCHEN','BEDROOM','BATHROOM','LIVING'];
 BEGIN
-    while i < 100000 loop
+    while i < 1000000 loop
             insert into room(house_id, area_size, height,room_type) VALUES (i+1,19,3,rooms[i%4+1]),
                                                                    (i+1,22,3,rooms[i%4+1]),
                                                                    (i+1,22,3,rooms[i%4+1]);
@@ -148,10 +148,10 @@ DECLARE
     i int = 0;
     st script_type[] = array ['CONDITIONAL','SCHEDULE'];
 BEGIN
-    while i < 10000000 loop
-            if i < 5000000 then
+    while i < 1000000 loop
+            if i < 500000 then
                 insert into script(creator, script_type)  VALUES (i%1000+1,st[1]) ;
-            elsif i < 10000000 then
+            elsif i < 1000000 then
                 insert into script(creator, script_type)  VALUES (i%1000+1,st[2]) ;
             end if;
             i = i + 1;
@@ -160,3 +160,54 @@ BEGIN
 end;
 $$ language plpgsql;
 select fill_script();
+
+create or replace function fill_condition_script() returns setof condition_script as $$ DECLARE
+    i int = 0; BEGIN
+    while i < 500000 loop
+            insert into condition_script(script_id, condition_text) VALUES (i+1,'click button') ; i = i + 1;
+        end loop;
+    return query select * from condition_script limit 500;
+end;
+$$ language plpgsql;
+select fill_condition_script();
+
+create or replace function fill_schedule_script() returns setof schedule_script as $$ DECLARE
+    i int = 0; BEGIN
+    while i < 500000 loop
+        insert into schedule_script(script_id, action_time, repeat_on_monday, repeat_on_tuesday, repeat_on_wednesday, repeat_on_thursday, repeat_on_friday, repeat_on_saturday, repeat_on_sunday) VALUES
+            (i+50001,'8:00',true,true,true,true,true,false,false) ;
+        i = i + 1;
+        end loop;
+    return query select * from schedule_script limit 500;
+end;
+$$ language plpgsql;
+select fill_schedule_script();
+
+create or replace function fill_list_action_script() returns setof list_action_script as $$ DECLARE
+    i int = 0; BEGIN
+    while i < 1000000 loop
+            insert into list_action_script(script_id, action_id) values (i+1,1),(i+1,2),(i+1,3); i = i + 1;
+        end loop;
+    return query select * from list_action_script limit 500;
+end;
+$$ language plpgsql;
+select fill_list_action_script();
+
+create or replace function fill_list_user_house() returns setof list_user_house as $$ DECLARE
+    i int = 0; BEGIN
+    while i < 1000000 loop
+            insert into list_user_house(user_id, house_id) values (i + 1, i+1); i = i + 1;
+        end loop;
+    return query select * from list_user_house limit 500; end;
+$$ language plpgsql;
+select fill_list_user_house();
+
+create or replace function fill_list_script_user() returns setof list_script_user as $$ DECLARE
+    i int = 0; BEGIN
+    while i < 100000 loop
+            insert into list_script_user(user_id, script_id) values (i + 1,i*10+1),(i + 1,i*10+2),(i +
+                                                                                                   1,i*10+3),(i + 1,i*10+4),(i + 1,i*10+5),(i + 1,i*10+6),(i + 1,i*10+7),(i + 1,i*10+8),(i + 1,i*10+9),(i + 1,i*10+10);
+            i = i + 1; end loop;
+    return query select * from list_script_user limit 500; end;
+$$ language plpgsql;
+select fill_list_script_user();
